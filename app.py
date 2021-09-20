76e43627ce46da46871f17c12e4b62056f4047b2
Robert Rozehnal <rob.kab.roz@gmail.com>
2021-09-20 20:30:40 +0200

    added file app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class Calculator(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+
+
+root = Calculator()
+
+root.mainloop()
