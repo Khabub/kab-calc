436a82981c5e0e506ad9d84697cda8792d875020
Robert Rozehnal <rob.kab.roz@gmail.com>
2021-09-22 14:56:41 +0200

    making buttons

--- app.py
+++ app.py
@@ -42,25 +42,226 @@ class Calculator(tk.Tk):
         container.rowconfigure(0, weight=1)
         container.columnconfigure(0, weight=1)
 
-        row = 0
-        for rows in calc:
-            col = 0
-            for key, value in rows.items():
-                button = tk.Button(
-                    keypad,
-                    text=key,
-                    height=1,
-                    width=4,
-                    font=("Segoe UI bold", 20),
-                    foreground=value[1],
-                    command=self.addval(value[0])
-                )
-                button.grid(row=row, column=col, columnspan=value[0], padx=2, pady=2)
-                col += value[0]
-            row += 1
+        # Keypad buttons
+        button_01 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
 
-        but = ttk.Button(container, command=self.addval)
-        but.grid(row=2, column=1)
+        )
+        button_01.grid(row=1, column=0, padx=2, pady=2)
+
+        button_02 = tk.Button(
+            keypad,
+            text="NA",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_02.grid(row=1, column=1, padx=2, pady=2)
+
+        button_03 = tk.Button(
+            keypad,
+            text="NA",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_03.grid(row=1, column=2, padx=2, pady=2)
+
+        button_04 = tk.Button(
+            keypad,
+            text="DEL",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_04.grid(row=1, column=3, padx=2, pady=2)
+
+        button_05 = tk.Button(
+            keypad,
+            text="7",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_05.grid(row=2, column=0, padx=2, pady=2)
+
+        button_06 = tk.Button(
+            keypad,
+            text="8",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_06.grid(row=2, column=1, padx=2, pady=2)
+
+        button_07 = tk.Button(
+            keypad,
+            text="9",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_07.grid(row=2, column=2, padx=2, pady=2)
+
+        button_08 = tk.Button(
+            keypad,
+            text="/",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_08.grid(row=2, column=3, padx=2, pady=2)
+
+        button_09 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_09.grid(row=1, column=0, padx=2, pady=2)
+
+        button_10 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_10.grid(row=1, column=0, padx=2, pady=2)
+
+        button_11 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_11.grid(row=1, column=0, padx=2, pady=2)
+
+        button_12 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_12.grid(row=1, column=0, padx=2, pady=2)
+
+        button_13 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_13.grid(row=1, column=0, padx=2, pady=2)
+
+        button_14 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_14.grid(row=1, column=0, padx=2, pady=2)
+
+        button_15 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_15.grid(row=1, column=0, padx=2, pady=2)
+
+        button_16 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_16.grid(row=1, column=0, padx=2, pady=2)
+
+        button_17 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_17.grid(row=1, column=0, padx=2, pady=2)
+
+        button_18 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_18.grid(row=1, column=0, padx=2, pady=2)
+
+        button_19 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_19.grid(row=1, column=0, padx=2, pady=2)
+
+        button_20 = tk.Button(
+            keypad,
+            text="C",
+            height=1,
+            width=4,
+            font=("Segoe UI bold", 20),
+            foreground=BLACK,
+
+        )
+        button_20.grid(row=1, column=0, padx=2, pady=2)
 
     def addval(self, v):
 
